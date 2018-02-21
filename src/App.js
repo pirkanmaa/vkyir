8de0cc30bb53a8b175ad4eed80648f21ec2c1bf4
Ilpo Tammi <33652320+iltapommi@users.noreply.github.com>
2018-02-21 09:32:54 +0200

    Typo fix

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     render() {
         return (
             <div className='app'>
-                <h1 className='header'}> App container </h1>
+                <h1 className='header'> App container </h1>
                 <MapTheme></MapTheme>
                 <Map></Map>
                 <Toolbar></Toolbar>
