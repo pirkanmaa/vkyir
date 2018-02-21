db2288b38956f17c7b75f5524e813c65ae698ba5
Ilpo Tammi <33652320+iltapommi@users.noreply.github.com>
2018-02-21 17:10:31 +0200

    Wrapped app into Material UI theme (required)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
 import Map from './components/Map';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import LayerControl from './components/LayerControl';
 import Toolbar from './components/Toolbar';
 
 class App extends Component {
-    constructor(){
-    	super();
-    	this.state = {
+    constructor() {
+        super();
+        this.state = {
             number: 123,
         };
     }
 
     render() {
         return (
+            <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
             <div className='app'>
                 <Map></Map>
                 <LayerControl></LayerControl>
                 <Toolbar></Toolbar>
             </div>
+            </MuiThemeProvider>
         );
     }
 }
-
 export default App;
