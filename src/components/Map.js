af630edd09c5051be51c76194ea91e2a2fbd1e73
Ilpo Tammi <33652320+iltapommi@users.noreply.github.com>
2018-02-21 13:44:18 +0200

    Added "store" for default view values

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,6 +5,16 @@ import TileLayer from 'ol/layer/tile';
 import XYZ from 'ol/source/xyz';
 import Zoom from 'ol/control/zoom';
 
+const defaults = {
+    center: [1100000, 7600000],
+    zoom: 7
+}
+
+const basemaps = {
+    osm: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    mapboxGray: 'https://api.mapbox.com/styles/v1/webigu/cjdwtqlgj7dev2snnlo0nfaiu/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoid2ViaWd1IiwiYSI6ImNqZHd0cTNidzBvM2kyeHM2Mjh2YzdiMGoifQ.3fbmDT3SZof-RM3uSpHMDg'
+}
+
 class Map extends Component {
     constructor() {
         super();
@@ -16,14 +26,13 @@ class Map extends Component {
             layers: [
                 new TileLayer({
                     source: new XYZ({
-                        //url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-                        url: 'https://api.mapbox.com/styles/v1/webigu/cjdwtqlgj7dev2snnlo0nfaiu/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoid2ViaWd1IiwiYSI6ImNqZHd0cTNidzBvM2kyeHM2Mjh2YzdiMGoifQ.3fbmDT3SZof-RM3uSpHMDg'
+                        url: basemaps.mapboxGray
                     })
                 })
             ],
             view: new View({
-                center: [1100000, 7600000],
-                zoom: 7
+                center: defaults.center,
+                zoom: defaults.zoom
             }),
             controls: [new Zoom]
         });
