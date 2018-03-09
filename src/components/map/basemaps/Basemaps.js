db7019c4e23039625747999269c0abbfb32ac363
Ilpo Tammi <33652320+iltapommi@users.noreply.github.com>
2018-03-09 13:35:35 +0200

    Comments

--- src/components/map/basemaps/Basemaps.js
+++ src/components/map/basemaps/Basemaps.js
@@ -4,10 +4,10 @@ import MapboxLight from './MapboxLight';
 import MapboxDark from './MapboxDark';
 import MapboxSatellite from './MapboxSatellite';
 
-// Choose basemaps
+/* Choose basemaps */
 let Basemaps = [OSM, CartoLight, MapboxLight, MapboxDark, MapboxSatellite];
 
-// This converts the above basemap array into an array of objects with keys "layer, name, title" with values from map layers
+/* This converts the above basemap array into an array of objects with keys "layer, name, title" with values from map layers */
 function convert(array) {
     return array.reduce((acc, layer) => {
         acc.push({
@@ -15,7 +15,7 @@ function convert(array) {
             name: layer.getProperties().name,
             title: layer.getProperties().title
         }); return acc;
-    },[])
+    }, [])
 }
 
 export default Basemaps = convert(Basemaps);
