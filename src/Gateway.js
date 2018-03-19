a1ec46ad57d02217fec6664f14023343ddf38634
Ossi Tammi <ossi.tammi@gmail.com>
2018-03-19 20:15:36 +0200

    Added comments on Gateway component

--- src/Gateway.js
+++ src/Gateway.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import App from './App';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+/* Gateway -component works as a proxy between App and outer realms */
 class Gateway extends Component {
     render() {
         return (
+            /* In the future there might be other resources other than the root */
             <Router>
                 <Route exact path='/' component={App}/>
             </Router>
