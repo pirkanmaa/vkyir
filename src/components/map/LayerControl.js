55e8b086f8a2441c07265185912e6e1a6e15d246
Ilpo Tammi <33652320+iltapommi@users.noreply.github.com>
2018-06-04 09:16:57 +0300

    Updated layer controls - beta

--- src/components/map/LayerControl.js
+++ src/components/map/LayerControl.js
@@ -3,55 +3,128 @@ import { withStyles } from 'material-ui/styles';
 import Checkbox from 'material-ui/Checkbox';
 import { FormLabel, FormControl, FormGroup, FormControlLabel } from 'material-ui/Form';
 import Layers from './layers/Layers';
+import { UserContext } from '../../App';
+import Clear from 'material-ui-icons/Clear';
+import IconButton from 'material-ui/IconButton';
+import Button from 'material-ui/Button';
+import LayerAdder from './LayerAdder';
+import Add from 'material-ui-icons/Add';
 
-const styles = {
+const styles = theme => ({
   root: {
     display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
     paddingLeft: '1rem',
     paddingTop: '1rem',
+  },
+  superContainer: {
+    width: '250px'
+  },
+  button: {
+    position: 'absolute',
+    right: '8px'
+  },
+  addButton: {
+    paddingLeft: '14px'
+  },
+  leftIcon: {
+    marginRight: theme.spacing.unit,
   }
-}
+})
 
 class LayerControl extends Component {
 
   state = {
-    visibility: Layers.map((item, index) => item.visibility)
+    show: { "visibility": Layers.map(item => item.visibility), "title": Layers.map(item => item.title) },
+    showLayerAdder: false
   }
+
+  toggleLayerAdder = () => this.setState({ showLayerAdder: !this.state.showLayerAdder });
+  closeLayerAdder = () => this.setState({ showLayerAdder: false });
+
+  toggleVisibility = event => {
+    this.props.toggleLayer(event)
+    let index = this.state.show.title.findIndex(title => title === event.target.value);
+    let show = Object.assign({}, this.state.show);
+    show.visibility = show.visibility.map((item, i) => { return i === index ? !item : item });
+    this.setState({ show });
+    console.log(this.state.show);
+  }
+
+  removeLayer = buttonName => {
+    let layer = this.props.map.getLayers().getArray().filter(layer => layer.getProperties().title === buttonName);
+    let index = this.state.show.title.findIndex(title => title === buttonName);
+
+    this.setState({
+      show: {
+        "visibility": this.state.show.visibility.filter((item, i) => i !== index),
+        "title": this.state.show.title.filter((item, i) => i !== index)
+      }
+    });
+
+    this.props.map.removeLayer(...layer);
+  };
 
   render() {
 
     const { classes } = this.props;
 
     return (
-      <div>
-        <FormControl component="fieldset" required classes={{ root: classes.root }}>
-          <FormGroup
-            aria-label="layers"
-            name="layers"
-          >
-            {Layers.map((item, index) => (
-              <FormControlLabel
-                key={index}
-                label={item.title}
-                control={
-                  <Checkbox
-                    key={index}
-                    checked={this.state.visibility[index]}
-                    value={item.name}
-                    onChange={(event) => {
-                      this.props.toggleLayer(event)
-                      this.setState({visibility: this.state.visibility.map(function(item, i) { return i === index ? !item : item })})
-                    }}
-                  />
-                }
-              />
-            ))}
-          </FormGroup>
-        </FormControl>
-      </div>
+      <UserContext.Consumer>
+        {user => {
+          return (
+            <div className={classes.superContainer}>
+              <FormControl component="fieldset" required classes={{ root: classes.root }}>
+                <FormGroup
+                  aria-label="layers"
+                  name="layers"
+                >
+                  {Layers.map((item, index) => (
+                    this.state.show.title.indexOf(item.title) > -1 &&
+                    <div key={index}>
+                      <FormControlLabel
+                        key={'A' + index}
+                        label={item.title}
+                        control={
+                          <Checkbox
+                            key={'B' + index}
+                            checked={this.state.show.visibility[this.state.show.title.indexOf(item.title)]}
+                            value={item.name}
+                            onChange={this.toggleVisibility}
+                          />
+                        }
+                      />
+                      {
+                        user && user !== '' && (
+                          <IconButton value={item.title} key={'C' + index} className={classes.button} onClick={() => this.removeLayer(item.title)}>
+                            <Clear />
+                          </IconButton>
+                        )
+                      }
+                    </div>
+                  ))}
+                </FormGroup>
+              </FormControl>
+
+              {user && user !== '' && (
+
+                <div>
+                  <Button onClick={this.toggleLayerAdder} className={classes.addButton}>
+                    <Add className={classes.leftIcon} />Add map layer
+                    </Button>
+                  <LayerAdder
+                    showLayerAdder={this.state.showLayerAdder}
+                    closeLayerAdder={this.closeLayerAdder}
+                    setData={this.props.setData}
+                    map={this.props.map}>
+                  </LayerAdder>
+                </div>
+              )
+              }
+
+            </div>
+          )
+        }}
+      </UserContext.Consumer>
     );
   }
 }
--- src/components/map/LayerDrawer.js
+++ src/components/map/LayerDrawer.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
 import Drawer from 'material-ui/Drawer';
+import Button from 'material-ui/Button';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
 import { withStyles } from 'material-ui/styles';
 import { MenuItem } from 'material-ui/Menu';
 import BasemapControl from './BasemapControl';
 import LayerControl from './LayerControl';
 import Divider from 'material-ui/Divider';
+import ExpansionPanel, {
+  ExpansionPanelDetails,
+  ExpansionPanelSummary,
+} from 'material-ui/ExpansionPanel';
+import ExpandMore from 'material-ui-icons/ExpandMore';
+import Layers from 'material-ui-icons/Layers';
 
 const styles = {
   paper: {
     width: '250px'
   },
   root: {
-    marginTop: '1rem',
-    marginBottom: '1rem'
+    paddingLeft: '8px',
+    paddingTop: '0px',
+    paddingRight: '0px'
   }
 }
 
 class LayerDrawer extends Component {
+
+  state = {
+    expanded: 'panel',
+  };
+
+  handleChange = panel => (event, expanded) => {
+    this.setState({
+      expanded: expanded ? panel : false,
+    });
+  };
+
   render() {
 
     const { classes } = this.props;
+    const { expanded } = this.state;
 
     return (
       <div>
@@ -28,17 +51,33 @@ class LayerDrawer extends Component {
           variant='persistent'
           anchor='left'
           open={this.props.layerDrawerVisibility}>
-          <LayerControl
-            maplayers={this.props.maplayers}
-            toggleLayer={this.props.toggleLayer}
-          />
-          <Divider
-            classes={{ root: classes.root }}
-          />
-          <BasemapControl
-            changeBasemap={this.props.changeBasemap}
-            basemap={this.props.basemap}
-          />
+
+          <ExpansionPanel defaultExpanded expanded={expanded === 'panel'} onChange={this.handleChange('panel')}>
+            <ExpansionPanelSummary expandIcon={<ExpandMore />}>
+              <Typography>Map layers</Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails classes={{ root: classes.root }}>
+              <LayerControl
+                maplayers={this.props.maplayers}
+                toggleLayer={this.props.toggleLayer}
+                setData={this.props.setData}
+                map={this.props.map}
+              />
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+
+          <ExpansionPanel defaultExpanded expanded={expanded === 'panel'} onChange={this.handleChange('panel')}>
+            <ExpansionPanelSummary expandIcon={<ExpandMore />}>
+              <Typography>Basemaps</Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails classes={{ root: classes.root }}>
+              <BasemapControl
+                changeBasemap={this.props.changeBasemap}
+                basemap={this.props.basemap}
+              />
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+
         </Drawer>
       </div>
     );
