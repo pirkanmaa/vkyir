fe524a1c0c84ebd6ce66fd28e4afc9bd561cae9d
ZenJappanen <jans@pirkanmaa.fi>
2019-04-29 08:42:23 +0300

    removed logging

--- src/components/map/layers/Layers.js
+++ src/components/map/layers/Layers.js
@@ -73,7 +73,7 @@ let Layers = [
 /* This converts the above basemap array into an array of objects with keys "layer, name, title" with values from map layers */
 function convert(array) {
   return array.reduce((acc, layer) => {
-    console.log(layer.getProperties().group);
+    //console.log(layer.getProperties().group);
     acc.push({
       layer: layer,
       name: layer.getProperties().name,
